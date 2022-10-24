172212bbec901fee1f071c13eb85edd0da2beebc
Sweta Mishra <m.swetab14@gmail.com>
2022-10-24 16:20:49 +0800

    Made minor changes

--- ui/src/AuthHeader.jsx
+++ ui/src/AuthHeader.jsx
@@ -1,10 +1,15 @@
 import React , {useEffect, useState} from 'react';
 import { GoogleLogin , GoogleLogout } from 'react-google-login';
-import { gapi} from 'gapi-script'
+import { gapi} from 'gapi-script';
 
 export default function AuthHeader(){
 
-    const clientId="559196906501-bqac4bfhiuakmnp53csn2m4epfm1eo6v.apps.googleusercontent.com"
+    const clientId="559196906501-bqac4bfhiuakmnp53csn2m4epfm1eo6v.apps.googleusercontent.com";
+    useEffect( () => {
+        gapi.load("client:auth2",()=> {
+            gapi.auth2.init({clientId:clientId})
+        })
+    },[])
     const [user , setuser] = useState();
     const logout = () => {
         console.log("Logged Out");
@@ -12,32 +17,25 @@ export default function AuthHeader(){
     }
     const success = (response) => {
         console.log("Login Success");
-        console.log(response);
         setuser(response.wt.Ad);
     }
-    const failure = (response) => {
+    const failure = ( response ) => {
         console.log("Login Failure");
         console.log(response);
     }
-    useEffect( () => {
-        gapi.load("client:auth2",()=> {
-            gapi.auth2.init({clientId:clientId})
-        })
-    },[])
-    return (
-        <header>
+    return (<header>
         <GoogleLogin
-            clientId = {clientId} 
+            clientId={clientId}
             buttonText="Login"
             onSuccess={success}
             onFailure={failure}
         />
+        {/**cookiePolicy={'single_host_origin'}*/}
         <GoogleLogout
             clientId={clientId}
             buttonText="Logout"
             onLogoutSuccess={logout}
         />
-        {user && <h1>Hi , {user}</h1>}
-        </header>
-    );
+    {user && <h1>Hi , {user}</h1>}
+        </header>);
 }
